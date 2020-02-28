78a01e9851c04e7d6aa3f36cf1841049f54d2c71
sanilakumary <48491134+sanilakumary@users.noreply.github.com>
2020-02-28 02:18:37 +0100

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductMicroService.DBContexts;
+using ProductMicroService.Model;
+using ProductMicroService.Repository;
+
+namespace ProductMicroService.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        // GET: api/Product
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = _productRepository.GetProducts();
+            return new OkObjectResult(products);
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var product = _productRepository.GetProductByID(id);
+            return new OkObjectResult(product);
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            using (var scope = new TransactionScope())
+            {
+                _productRepository.InserProduct(product);
+                scope.Complete();
+                return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+            }
+        }
+
+        // PUT: api/Product/5
+        [HttpPut]
+        public IActionResult Put([FromBody] Product product)
+        {
+            if(product!= null)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    _productRepository.UpdateProduct(product);
+                    scope.Complete();
+                    return new OkResult();
+                }
+                
+            }
+            return NoContentResult();
+        }
+
+        private IActionResult NoContentResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
