6f82279c7fa7adf48f4ef9b97d13f63db0565476
sanilakumary <48491134+sanilakumary@users.noreply.github.com>
2020-02-28 02:20:54 +0100

    Add files via upload

---
+++ IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductMicroService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroService.Repository
+{
+   public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductByID(int ProductId);
+        void InserProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int ProductId);
+        void Save();
+
+    }
+}
---
+++ ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using ProductMicroService.DBContexts;
+using ProductMicroService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroService.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _productContext;
+        public ProductRepository(ProductContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        public void DeleteProduct(int ProductId)
+        {
+            var products = _productContext.Products.Find(ProductId);
+
+            _productContext.Products.Remove(products);
+            
+        }
+
+         public Product GetProductByID(int ProductId)
+        {
+            return _productContext.Products.Find(ProductId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _productContext.Products.ToList();
+        }
+
+         public void InserProduct(Product product)
+        {
+            _productContext.Products.Add(product);
+            Save();
+        }
+
+        
+
+        public void Save()
+        {
+            _productContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _productContext.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            Save();
+        }
+    }
+}
